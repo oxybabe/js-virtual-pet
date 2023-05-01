b26412cd6d2ec06c3474528ed5e95fbfdb9f905d
Elia Partin <arenaselia@gmail.com>
2023-05-01 14:33:37 -0400

    added thirst button added friend personality age

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,24 @@
 let petName = "Teetee";
 let hungerlevel = 50;
 let mood = "Sad";
+let personality = "Playful";
+let thirstlevel = 50;
+let friend = "Kiko the dog";
+let age = "19";
 
 //Get DOM elements for the pet's status
 const petNameEl = document.querySelector("#pet-name");
 const hungerEl = document.querySelector("#hunger-level");
 const moodEl = document.querySelector("#mood");
+const personalityEl = document.querySelectorAll("#playful");
+const thirstEl = document.querySelectorAll("#thirst-level");
+const friendEl = document.querySelector("#friend");
+const age = document.querySelector("#age");
 
 //Get DOM elements for the action buttons
 const feedButton = document.querySelector("#feed-button");
 const playButton = document.querySelector("#play-button");
+const drinkButton = document.querySelector("#drink-button");
 
 function feedPet() {
  if(hungerLevel > 10) {
@@ -25,7 +34,10 @@ function feedPet() {
     mood = "Happy";
     moodEl.innerText = mood;
  }
-}
+ 
+   }
+
 
 feedButton.addEventListener("click", feedPet);
+
 
